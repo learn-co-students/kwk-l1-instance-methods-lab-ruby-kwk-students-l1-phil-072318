085bedbc6f739505a29d6aae50fab6f14b9ec51b
Jennifer Okwara <jokwara03@gmail.com>
2018-07-26 20:33:39 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,24 @@
 # person.rb
+class Person 
+   def initialize(talk, walk)
+    @talk = talk
+     @sit = sit
+   end
+  def return_talk
+    @talk
+    puts "Hello world!"
+   end
+ def return_walk
+   @walk
+   puts "The person is walking."
+ end
+ end
+  
+  chloe = Person.new("Chloe")
+  
+puts chloe.return_talk
+puts chloe.return_walk
+  
+  
+ 
+ 
