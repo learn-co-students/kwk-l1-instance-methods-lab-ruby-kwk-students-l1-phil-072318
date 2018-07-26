995c06f1ea128d1541e4132a0e85e9baf50a1808
eslawek <ellieslawek@icloud.com>
2018-07-26 19:03:41 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,25 @@
 # dog.rb
+class Dog 
+
+  def initialize(bark,sit)
+    @bark = bark
+    @sit = sit
+  end #def initialize name
+  
+  def bark
+     puts "Woof!"
+     puts @bark
+  end #bark
+  
+  def sit
+    puts "The Dog is sitting" 
+  end #sit
+end #class dog
+
+Henry = Dog.new("Henry", 5)
+
+
+Henry.bark
+Henry.sit
+
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,22 @@
 # person.rb
+ class Person 
+   
+   def initialize(talk, walk)
+    @talk = talk
+    @walk = walk
+  end #def initialize
+  
+  def talk
+    puts "Hello World!"
+  end #def talk
+  
+  def walk
+    puts "The Person is walking"
+  end #def walk
+  
+end #class person
+
+Anum=Person.new(1,2)
+
+Anum.talk
+Anum.walk
